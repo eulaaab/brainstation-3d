c191bbbec9459f06e5258621730e73c6569b10df
Eula Bengco <eulabengco@gmail.com>
2020-03-06 15:40:41 -0500

    first

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import React from "react";
+import "./App.scss";
+import Header from "./component/Header/header";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+import Body from "./component/Body/Body"
 
-import { Helmet } from 'react-helmet';
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
-    <div className="App">
+    <>
       {/* NOTE: Keep the helmet code below and change the app title.
           Add any meta tags you want, or any tag you want to go in the header. 
           You can modify everything else! */}
       <Helmet>
-        <title>Your App Title Here</title>
+        <title>StarTech</title>
       </Helmet>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h2>Brainstation 3D Starter</h2>
-        <h3>TEAM 3</h3>
-      </header>
-    </div>
+      <Router>
+        <Header />
+
+        <Switch>
+          {/* <Route path="/" exact component={Home} />
+            <Route path="/about" component={About} /> */}
+          {/* <Route path="/videos/:id" component={HomePage} /> */}
+
+        </Switch>
+      </Router>
+      <Body />
+    </>
   );
 }
 
---
+++ client/src/component/Body/Body.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+
+import "./Body.scss";
+
+
+
+class Body extends Component {
+
+  render() {
+    return (
+      <>
+        {
+          /*
+          <section className="hero">
+            <div className="hero__left">
+              <h2 className="hero__title">IS ICT FOR YOU? (Information and Communication Technology)</h2>
+              <p className="hero__slogan">
+                How company uses technology for their business
+                Target Demographics: New Grads
+                New Grads care? Where to work, which industr
+    </p>
+    
+            </div>
+            <div className="hero__right hero__image"><img src={undergrad} className="undergrad" />
+            </div>
+          </section>
+    */
+        }
+      </>
+    );
+  }
+}
+
+
+export default Body;
+
+
